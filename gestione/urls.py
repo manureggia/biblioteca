374c628010ea706a0003ff43468c11910da9ed5b
manu <emanuele.s.reggiani@gmail.com>
2024-04-13 17:28:39 +0200

    Ora funziona tutto

--- gestione/urls.py
+++ gestione/urls.py
@@ -5,10 +5,12 @@ from django.urls import path, include
 from gestione.views import lista_libri, mattoni, dettaglio_libro, edita_libro, crea_libro, presta_libro
 
 urlpatterns = [
+    path('', lista_libri, name="lista"),
     path('lista/', lista_libri, name="lista"),
     path('mattoni/', mattoni, name="mattoni"),
     path('detail/<int:id>', dettaglio_libro, name="dettaglio"),
     path('edit/<int:id>', edita_libro, name="edita"),
     path('aggiungi/', crea_libro, name="crea_libro"),
     path('prestito/<int:id>', presta_libro, name="prestito")
+
 ]
